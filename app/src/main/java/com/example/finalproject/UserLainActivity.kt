09144486058a01e5f06fa0ae0630bcbd586e8d19
dimasmmr <dimasrhmt.19@gmail.com>
2023-12-25 23:05:08 +0700

    tampil gambar user lain (dimas)

--- app/src/main/java/com/example/finalproject/UserLainActivity.kt
+++ app/src/main/java/com/example/finalproject/UserLainActivity.kt
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
 class UserLainActivity : AppCompatActivity() {
+    private lateinit var database: FirebaseDatabase
     private lateinit var userId: String
     private lateinit var usernameTextView: TextView
     private lateinit var emailTextView: TextView
@@ -29,6 +32,7 @@ class UserLainActivity : AppCompatActivity() {
 
                 usernameTextView.text = username
                 emailTextView.text = email
+                fetchUserImages(userId)
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
@@ -37,4 +41,57 @@ class UserLainActivity : AppCompatActivity() {
             }
         })
     }
+    // Di dalam UserLainActivity
+    private fun fetchUserImages(uid: String) {
+        val databaseReference = FirebaseDatabase.getInstance().reference.child("uploads")
+        val query = databaseReference.orderByChild("s").equalTo(uid)
+
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val imageList: MutableList<String> = mutableListOf()
+
+                for (snapshot in dataSnapshot.children) {
+                    val imageUrl = snapshot.child("imageUrl").getValue(String::class.java)
+                    imageUrl?.let { imageList.add(it) }
+                }
+
+                // Setelah mendapatkan daftar URL gambar, pasang ke dalam RecyclerView
+                val recyclerView: RecyclerView = findViewById(R.id.recyclerUserLain)
+                recyclerView.layoutManager = GridLayoutManager(this@UserLainActivity, 3)
+                val imageAdapter = UserLainAdapter(this@UserLainActivity, imageList)
+                recyclerView.adapter = imageAdapter
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle error saat mengambil data dari Firebase
+                Toast.makeText(this@UserLainActivity, "Failed to load user images.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
 }
+//    private fun fetchUserImages(uid: String) {
+//        val uploadsRef = database.reference.child("uploads").orderByChild("s").equalTo(uid)
+//
+//        uploadsRef.addListenerForSingleValueEvent(object : ValueEventListener {
+//            override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                val imageList: MutableList<String> = mutableListOf()
+//
+//                for (snapshot in dataSnapshot.children) {
+//                    val imageUrl = snapshot.child("imageUrl").getValue(String::class.java)
+//                    imageUrl?.let { imageList.add(it) }
+//                }
+//
+//                // Setelah mendapatkan daftar URL gambar, pasang adapter RecyclerView
+//                val recyclerView: RecyclerView = findViewById(R.id.recyProfil)
+//                recyclerView.layoutManager = GridLayoutManager(this@UserLainActivity, 3) // Atur layout manager dengan 3 kolom
+//                val imageAdapter = ImageUserAdapter(this@UserLainActivity, imageList)
+//                recyclerView.adapter = imageAdapter
+//            }
+//
+//            override fun onCancelled(databaseError: DatabaseError) {
+//                // Handle error saat mengambil data dari Firebase
+//                Toast.makeText(this@UserLainActivity, "Failed to read user images.", Toast.LENGTH_SHORT).show()
+//            }
+//        })
+//    }
---
+++ app/src/main/java/com/example/finalproject/UserLainAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.finalproject
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class UserLainAdapter(private val context: Context, private val imageList: List<String>) :
+    RecyclerView.Adapter<UserLainAdapter.ImageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_image_user_lain, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val imageUrl = imageList[position]
+        holder.bind(imageUrl)
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+
+    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView: ImageView = itemView.findViewById(R.id.imageViewItem)
+
+        fun bind(imageUrl: String) {
+            // Load gambar menggunakan Glide atau library serupa
+            Glide.with(context)
+                .load(imageUrl)
+                .into(imageView)
+        }
+    }
+}
